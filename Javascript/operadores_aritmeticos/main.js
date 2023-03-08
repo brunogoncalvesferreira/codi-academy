023cd01e51ade0c2d864cb53a338710fe3cc0e3f
Bruno Gonçalves Ferreira <brungoncalvesferreira@outlook.com>
2023-03-08 19:11:53 -0300

    :boom: update curso

---
+++ Javascript/operadores_aritmeticos/main.js
@@ -0,0 +1,59 @@
+// Operadores Aritméticos
+/*
+adição + 
+subtração - 
+multiplcação *
+divisão /
+resto da divisão %
+*/
+
+// Adição
+let sum = 1 + 3
+console.log(sum)
+sum += 1
+console.log(sum)
+let outherSum = sum + 1
+console.log(outherSum)
+
+// Subtração
+let sub = 3 - 1
+console.log(sub)
+sub -= 1
+console.log(sub)
+let outherSub = sub - 1
+console.log(outherSub)
+
+// Multiplicação
+let mult = 9 * 9
+console.log(mult)
+mult *= 9
+console.log(mult)
+let outherMult = mult * 9
+console.log(outherMult)
+
+// Divisão
+let div = 81 / 9
+console.log(div)
+div /= 9
+console.log(div)
+let outherDiv = div / 9
+console.log(outherDiv)
+
+// Resto da divisão
+let rest = 81 % 9
+console.log(rest)
+let outherRest = 99 % 26
+console.log(outherRest)
+
+// Exe
+let mulher = "Kelly"
+let precoDoBiscoito = 2
+let quantidadeComprar = 10
+let valorPago = precoDoBiscoito * quantidadeComprar
+console.log(mulher, "pagou R$", valorPago)
+
+// Exe
+let numeroAlunos = 100
+let alunosMasculinosPorcentagem = 53
+let totalDeAlunosSexoMasculinos = alunosMasculinosPorcentagem % numeroAlunos
+console.log(totalDeAlunosSexoMasculinos)
---
+++ Javascript/variaveis/main.js
@@ -0,0 +1,34 @@
+// Variáveis
+
+/* Tipos de dados
+  - string
+  - number
+  - boolean
+  - undefined
+  - null
+*/
+
+// Javascript é uma linguegem de programação dinâmica, pois nçao precisamos declarar o tipo de dado a variável.
+
+// Nomeclatura
+// 1. Não pode começar com número
+// 2. Nomes pode conter => letras, numeros, e _ (underline)
+// 3. Não pode conter palavras reservadas
+
+// Declaração
+let player
+
+// Atribuição
+player = "Mário"
+
+// Exibir
+console.log(player)
+
+let nome = "Bruno"
+console.log(nome)
+
+let taVivo = true
+console.log("Está vivo:", taVivo)
+
+let numero = 653.88
+console.log(numero)
